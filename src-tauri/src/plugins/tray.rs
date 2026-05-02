c5b07a022e5497432e49b4e3d165e5e3c1dff9ac
Karmicore <Karmicore@users.noreply.github.com>
2026-05-02 13:24:51 +0800

    feat:折叠展开单选类似滑块的感觉

--- src-tauri/src/plugins/tray.rs
+++ src-tauri/src/plugins/tray.rs
@@ -9,6 +9,8 @@ use tauri::{
 use crate::app_core::service::NoteService;
 use crate::commands::window_cmd;
 
+const COLLAPSED_HEIGHT: u32 = 28;
+
 pub struct TrayPlugin;
 
 impl TrayPlugin {
@@ -16,18 +18,36 @@ impl TrayPlugin {
         Self
     }
 
-    fn create_menu(app: &AppHandle) -> tauri::Result<Menu<tauri::Wry>> {
-        let show = MenuItem::with_id(app, "show", "显示全部", true, Some("Alt+S"))?;
-        let hide = MenuItem::with_id(app, "hide", "隐藏全部", true, Some("Alt+H"))?;
-        let collapse_all = MenuItem::with_id(app, "collapse_all", "折叠全部", true, Some("Alt+C"))?;
-        let expand_all = MenuItem::with_id(app, "expand_all", "展开全部", true, Some("Alt+E"))?;
-        let new_note = MenuItem::with_id(app, "new", "新建便签", true, Some("Alt+N"))?;
-        let quit = MenuItem::with_id(app, "quit", "退出", true, None::<&str>)?;
-        Menu::with_items(app, &[&show, &hide, &collapse_all, &expand_all, &new_note, &quit])
+    fn any_collapsed(app: &AppHandle) -> bool {
+        for (label, window) in app.webview_windows() {
+            if !label.starts_with("note-") {
+                continue;
+            }
+            if let Ok(size) = window.inner_size() {
+                if size.height <= COLLAPSED_HEIGHT + 2 {
+                    return true;
+                }
+            }
+        }
+        false
     }
 
     pub fn build(&self, app: &AppHandle) -> Result<(), String> {
-        let tray_menu = Self::create_menu(app).map_err(|e| e.to_string())?;
+        let show = MenuItem::with_id(app, "show", "显示全部", true, Some("Alt+S"))
+            .map_err(|e| e.to_string())?;
+        let hide = MenuItem::with_id(app, "hide", "隐藏全部", true, Some("Alt+H"))
+            .map_err(|e| e.to_string())?;
+        let toggle_collapse = MenuItem::with_id(app, "toggle_collapse", "折叠全部", true, None::<&str>)
+            .map_err(|e| e.to_string())?;
+        let new_note = MenuItem::with_id(app, "new", "新建便签", true, Some("Alt+N"))
+            .map_err(|e| e.to_string())?;
+        let quit = MenuItem::with_id(app, "quit", "退出", true, None::<&str>)
+            .map_err(|e| e.to_string())?;
+
+        let tray_menu = Menu::with_items(app, &[&show, &hide, &toggle_collapse, &new_note, &quit])
+            .map_err(|e| e.to_string())?;
+
+        let toggle_item = toggle_collapse.clone();
 
         let _tray = TrayIconBuilder::new()
             .icon(app.default_window_icon().unwrap().clone())
@@ -36,14 +56,15 @@ impl TrayPlugin {
             .on_menu_event(move |app, event| match event.id.as_ref() {
                 "show" => window_cmd::show_all_note_windows(app),
                 "hide" => window_cmd::hide_all_note_windows(app),
-                "collapse_all" => {
+                "toggle_collapse" => {
                     if let Some(svc) = app.try_state::<Arc<NoteService>>() {
-                        window_cmd::collapse_all_note_windows(app, &svc);
-                    }
-                }
-                "expand_all" => {
-                    if let Some(svc) = app.try_state::<Arc<NoteService>>() {
-                        window_cmd::expand_all_note_windows(app, &svc);
+                        if Self::any_collapsed(app) {
+                            window_cmd::expand_all_note_windows(app, &svc);
+                            toggle_item.set_text("折叠全部").ok();
+                        } else {
+                            window_cmd::collapse_all_note_windows(app, &svc);
+                            toggle_item.set_text("展开全部").ok();
+                        }
                     }
                 }
                 "new" => { app.emit("create-note", ()).ok(); }
